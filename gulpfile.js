236d555785c7dcf8912940cf025e36061cec69a0
Roman Agarkov <agarkovromachka@mail.ru>
2020-12-08 00:24:46 +0300

    add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
 const gulp = require('gulp')
 const plumber = require('gulp-plumber')
 const sourcemap = require('gulp-sourcemaps')
-const postcss = require('gulp-postcss')
+// const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
 const sync = require('browser-sync').create()
+const csso = require('gulp-csso')
+const rename = require('gulp-rename')
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
+const svgstore = require('gulp-svgstore')
+const del = require('del')
 
-const styles = () => {
+const css = () => {
     return gulp.src('source/css/initial/*.css')
         .pipe(plumber())
         .pipe(sourcemap.init())
-//         .pipe( postcss([ require('autoprefixer') ]) )
+        // .pipe(postcss([autoprefixer()]))
+        .pipe(csso())
+        .pipe(rename('styles.min.css'))
         .pipe(sourcemap.write('.'))
         .pipe(gulp.dest('source/css'))
         .pipe(sync.stream())
 }
 
-const server = () => {
+const browserSync = (done) => {
     sync.init({
         server: {
             baseDir: './'
@@ -22,14 +31,71 @@ const server = () => {
         cors: true,
         notify: false,
         ui: false,
+        port: 8000
     })
+    done()
 }
 
-const watcher = () => {
-    gulp.watch('source/css/initial/**/*.css', gulp.series('styles'))
-    gulp.watch('./*.html').on('change', sync.reload)
+const serverReload = (done) => {
+    sync.reload()
+    done()
 }
 
-exports.defaul = gulp.series(
-    styles, server, watcher
-)
+// const images = () => {
+//     return gulp.src('source/{img, icons}/**/*.{jpg,png,svg}')
+//         .pipe(imagemin([
+//             imagemin.optipng({optimizationLevel: 3}),
+//             imagemin.mozjpeg({progressive: true}),
+//             imagemin.svgo({
+//                 plugins: [
+//                     {removeViewBox: true},
+//                     {cleanupIDs: false}
+//                 ]
+//             })
+//         ]))
+// }
+
+// const gulpWebp = () => {
+//     return gulp.src('source/img/**/*.{jpg,png}')
+//         .pipe(webp({quality: 90}))
+//         .pipe(gulp.dest('source/img'))
+// }
+
+// const sprite = () => {
+//     return gulp.src('source/icons/**/icon-*.svg')
+//         .pipe(svgstore())
+//         .pipe(rename('sprite.svg'))
+//         .pipe(gulp.dest('source/icons'))
+// }
+
+// const clean = () => {
+//     return del('build')
+// }
+
+// const copy = () => {
+//     return gulp.src([
+//         "source/fonts/**/*.:{woff, woff2",
+//         "source/img/**",
+//         "source/files/**",
+//         "source/icons/**",
+//         "source/js/**",
+//         "source/*.ico"
+//     ], {base: 'source'}).pipe(gulp.dest('build'))
+// }
+
+// const build = gulp.series('clean', 'copy')
+
+function watchFiles() {
+    gulp.watch('source/css/initial/**/*.css', css);
+    gulp.watch('./*.html', serverReload);
+}
+
+
+const watch = gulp.parallel(watchFiles, browserSync)
+
+exports.watch = watch
+exports.css = css
+
+// exports.default = gulp.series(
+//     css, server, watcher
+// )
